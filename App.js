58ef623fdcc9ac235f8254e217a2f254536e8bcc
sorensei <merylsantiago21@gmail.com>
2021-11-24 14:45:47 -0600

    Persist Redux

--- App.js
+++ App.js
@@ -3,13 +3,20 @@ import Main from './components/MainComponent';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
+import { PersistGate } from 'redux-persist/es/integration/react';
+import Loading from './components/LoadingComponent';
 
-const store = ConfigureStore();
+const { persistor, store } = ConfigureStore();
 
 export default function App() {
   return (  // Pass the store to all of the app
     <Provider store={store}>  
-      <Main />
+      <PersistGate    // prevents app from rendering until redux store is rehydrating fully from client side
+                    loading={<Loading />}   // will show while redux store is rehydrating
+                    persistor={persistor}
+      >  
+        <Main />
+      </PersistGate>
     </Provider>
   );
 }
--- redux/configureStore.js
+++ redux/configureStore.js
@@ -4,18 +4,26 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
     'combineReducers' : reducing one single state object from createStore
     'applyMiddleware' : adds more redux functionality
 */
-import thunk from 'redux-thunk';            // dispatch and get state
-import logger from 'redux-logger';          // gives a traceable stack of user's action (good for errors)
+import thunk from 'redux-thunk';                    // dispatch and get state
+import logger from 'redux-logger';                  // gives a traceable stack of user's action (good for errors)
 // Objects array for server data
 import { campsites } from './campsites';
 import { comments } from './comments';
 import { promotions } from './promotions';
 import { partners } from './partners';
 import { favorites } from './favorites';
+import { persistStore, persistCombineReducers } from 'redux-persist';   // add persist support automatically updates when there's a change to state
+import storage from 'redux-persist/es/storage';     // access to local storage from our device
+
+const config = {    // 1st 2 props required
+    key: 'root',
+    storage,        // imported
+    debug: true     // redux persist console log messages to debug
+}
 
 export const ConfigureStore = () => {
     const store = createStore(
-        combineReducers({
+        persistCombineReducers(config, {    // 1st arg is the object we created will handle updating the state automatically from redux store
             campsites,
             comments,
             partners,
@@ -24,5 +32,7 @@ export const ConfigureStore = () => {
         }),
         applyMiddleware(thunk, logger)
     );
-    return store;
+
+    const persistor = persistStore(store);  // persistStore has redux store as arg
+    return {persistor, store};              // to access both in App.js
 };
